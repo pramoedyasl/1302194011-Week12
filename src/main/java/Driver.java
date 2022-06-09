dba46e8ca8dbe4b2dc7bc420f55b98acd5fd53f1
pramoedyasl <pramoedyasl@student.telkomuniversity.ac.id>
2022-06-09 08:30:27 -0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,16 +6,28 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Number of count:" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Number of count:" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Number of count:" + counter.getCount());
 		
+		counter.increaseBy(5);
+		
+		System.out.println("Number of count:" + counter.getCount());
+		
+		counter.decreaseBy(2);
+		
+		System.out.println("Number of count:" + counter.getCount());
+
+		counter.multipyBy(6);
+		
+		System.out.println("Number of count:" + counter.getCount());
+
 	}
 
 }
